ac79dc0cb23d795f684e2d33904ffb2b2b8d4572
mockqv <richardviniciusfs07@gmail.com>
2026-03-03 01:00:54 -0300

    :sparkles: feat: static login page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { Dashboard } from "./pages/Dashboard"
+import Login from "./pages/Login"
+// import Register from "./pages/Register"
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
       </Routes>
     </BrowserRouter>
   )
 }
-
-export default App
